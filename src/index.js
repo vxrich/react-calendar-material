3881d5a34be9a401ac5f1745c62fb25030121d69
Paulo McNally <paulomcnally@gmail.com>
2017-10-26 21:24:50 -0600

    User prop-types from module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './index.css';
 import ic_back from './ic_back.svg';
 import ic_forward from './ic_forward.svg';
