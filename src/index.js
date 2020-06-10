9ab073ad39e93b8046c275a5cc2f4fbd2acd16d2
vxrich <riccardo.grespan6@gmail.com>
2020-06-10 17:43:32 +0200

    Remove console log

--- src/index.js
+++ src/index.js
@@ -76,11 +76,7 @@ class Calendar extends Component {
     }
 
     baseClasses += opts.current ? "" : " non-current";
-    console.log(
-      "DAY OF MONTH ==>",
-      opts.date.getDate(),
-      this.props.eventsBool[opts.date.getDate() - 1]
-    );
+
     return (
       <div key={opts.key} className={baseClasses} style={containerStyle}>
         <div
